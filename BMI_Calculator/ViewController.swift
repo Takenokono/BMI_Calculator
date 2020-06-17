1c87f431d3f3c248f0b237dea13e4bcec112d41a
Takenobu <KTakenobu@mac.com>
2020-06-17 16:59:21 +0900

    First commit

--- BMI_Calculator/ViewController.swift
+++ BMI_Calculator/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hight: UITextField!
+    
+    @IBOutlet weak var weight: UITextField!
+    
+    @IBOutlet weak var BMItext: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.hight.keyboardType = UIKeyboardType.numberPad
+        self.weight.keyboardType = UIKeyboardType.numberPad
     }
 
+    @IBAction func button(_ sender: Any) {
+        let dhight = Double(hight.text!)
+        let dweight = Double(weight.text!)
+        let dhight2 = dhight! * dhight!
+        let bmi = String(dweight! / dhight2)
+        
+        BMItext.text = ("あなたのBMIは" + bmi + "です")
 
+    }
 }
-
---
+++ README.md
@@ -0,0 +1,3 @@
+ # This is BMI Calculator on IOS
+I made an app on IOS to calculate BMI.
+I'm tired to of making IOS app.
